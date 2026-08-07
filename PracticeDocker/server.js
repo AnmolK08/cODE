dbd3036e3f383a3d79b9d8d52152ea19f9783a9f
Anmol Kumar <mranmolsingh04@gmail.com>
2026-08-07 19:10:05 +0530

    learned about volumes

--- PracticeDocker/server.js
+++ PracticeDocker/server.js
@@ -7,6 +7,11 @@ app.get("/", (req, res) => {
     res.send("Hello Docker");
 });
 
+app.get("/user", (req, res) => {
+    res.send(`Hello User are you ...?`);
+});
+
+
 app.listen(3000, () => {
     console.log("Server Running");
 });
